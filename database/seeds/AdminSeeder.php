935a2ea9ed6e42da3667c91459fc992537d350b9
minhhv <hominh4078@gmail.com>
2023-02-09 10:57:28 +0700

    first commit

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Admin::class,5)->create();
+    }
+}
